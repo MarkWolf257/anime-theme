e17c98211f5777fa4a63603d081eaabb4d570e21
MarkWolf257 <sambhunathmeher@gmail.com>
2024-09-16 23:36:27 +0530

    g

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function updateTime() {
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
-    timeElement.textContent = `${hours}:${minutes}`;
+    timeElement.textContent = `${hours} : ${minutes}`;
 
     const options = { weekday: 'long', day: 'numeric', month: 'long' };
     dateElement.textContent = now.toLocaleDateString(undefined, options);
